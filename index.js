ea74f21397c4656c62d5d3b56d795f0e610412a2
Deneb Pulsipher <pulsipherd@gmail.com>
2018-08-20 10:44:18 -0600

    create App.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import React from 'react';
+import App from '/components/App';
 
-React.render(<div>Private Equity Matching Game</div>, document.body);
+React.render(<App />, document.body);
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    var greeting = 'Welcome to the Private Equity Matching Game!'
+    return (
+      <div>
+        {greeting}
+      </div>
+    );
+  }
+}
